d43a742bfc7ed727c305f051a8004bb2eed6317d
jellytrain <49096871+jellytrain@users.noreply.github.com>
2019-05-16 23:04:56 +0300

    space shooter zaidimas

--- main.cpp
+++ main.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+#include <list>
+
 
 using namespace std;
 
-void taisykles()
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+//ledzi judeti teminate naudojant kordinates
+void gotoxy(int x, int y)
 {
+	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPOS;
+	dwPOS.X = x; //pradeda nuo 0;
+	dwPOS.Y = y; //pradeda nuo 0;
+
+	SetConsoleCursorPosition(hCon, dwPOS);
 }
+
+void HideCursor()
+{//paslepia cursoriu
+	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	cci.dwSize = 1;
+	cci.bVisible = false;
+}
+
+void DrawWhiteSpace(int a_x, int a_y, int b_x, int b_y)
+{//padaryti specialia vieta terminate, baltos spalvos, kad galima butu matyti kur vykst visas zaidimas
+	for(int i = a_x; i < b_x; i++)
+	{
+		for(int j = a_y; i < b_y; j++)
+		{
+			gotoxy(i,j); printf(" ");
+		}
+	}
+}
+
+
 
 
 int main()
 {
-	cout << " ------------------------- SAVE THE EARTH ------------------------- " << endl;
-	cout << " -                   * * *                                        - " << endl;
-	cout << " -                 *       *                  #                   - " << endl;
-	cout << " -               *          *                 #                   - " << endl;
-	cout << " -             *             *               # #    #             - " << endl;
-	cout << " -            *             *      @     #### # #####             - " << endl;
-	cout << " -             *         *                   # #    #             - " << endl;
-	cout << " -              *        *                    #                   - " << endl;
-	cout << " -                *    *                      #                   - " << endl;
-	cout << " -                  *                                             - " << endl;
-	cout << " ------------------------------------------------------------------ " << endl;
-	cout << "belekas" << endl;
-	cout <<" ŽAIDIMO TAISYKLES " << endl;
-    return 0;
+	cout << "Hello" << endl;
+
+	return 0;
 }
